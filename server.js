2221214a6e7f55ee844c42f7a0bfbbafc9f14e60
BrandonxX <91372621+BrandonxX@users.noreply.github.com>
2025-04-23 12:57:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: '2048-on-host.vercel.app',
+    origin: 'https://2048-on-host.vercel.app',
   }));
 
 // Rate limiting
